517697233d4d926076ca2077b0f07f7c1034db33
WhatLuck <98862042+WhatLuck@users.noreply.github.com>
2023-01-18 23:00:50 -0800

    test

--- src/main/java/org/example/MyListener.java
+++ src/main/java/org/example/MyListener.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Collection;
 import java.util.EnumSet;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
@@ -11,13 +13,15 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.utils.FileUpload;
 
 public class MyListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         Message message = event.getMessage();
         String content = message.getContentRaw();
-        String[] command = content.split(" ");
+        String[] args = content.split(" ");
 
         if (content.equals("!balls")) {
             EmbedBuilder builder = new EmbedBuilder();
@@ -56,33 +60,37 @@ public class MyListener extends ListenerAdapter {
             }
         }
 
-        if (content.startsWith("!r34")&&!event.getAuthor().isBot()) {
-            String search = content.substring(5);
+        if (content.startsWith("!booru")&&!event.getAuthor().isBot()) {
+            String search = content.substring(7);
             search = search.replace(" ","+");
-            try {
-                PostData postData = PostData.getPostsFromE621(search,2);
-                String imageUrl = PostData.getUrl();
-                int imageId = PostData.getPostId();
-                EmbedBuilder builder = new EmbedBuilder();
+            String url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&api_key=5985fd8c50a9c969f32688ae7a6cf66a7485b0a2e678c85aae6f8ab91da89f35&user_id=1171000&tags=" + search + "+sort:random&limit=1";
+            if(!search.isBlank()){
+                try {
+                    PostData postData2 = PostData.parseXML(url);
+                    String imageUrl = PostData.getUrl();
+                    int imageId = PostData.getPostId();
+                    EmbedBuilder builder = new EmbedBuilder();
 
-                HttpURLConnection connection = (HttpURLConnection) new URL(imageUrl).openConnection();
-                connection.setRequestMethod("HEAD");
-                if (connection.getResponseCode() == 200 ) {
+                    HttpURLConnection connection = (HttpURLConnection) new URL(imageUrl).openConnection();
+                    connection.setRequestMethod("HEAD");
+                    if (connection.getResponseCode() == 200 ) {
+                        Button refresh = Button.primary("","new");
+                        builder.setImage(imageUrl)
+                                .setTitle("Image via gelbooru","https://gelbooru.com/index.php?page=post&s=view&id="+imageId)
+                                .setDescription("Searched Tag(s): " + search.replace("+",", "))
+                                .setColor(0x93C54B)
+                                .setFooter(event.getAuthor().getName(), event.getAuthor().getAvatarUrl());
 
-                    builder.setImage(imageUrl)
-                            .setTitle("Image via r34.xxx","https://rule34.xxx/index.php?page=post&s=view&id="+imageId)
-                            .setDescription("Searched Tag(s): " + search.replace("+",", "))
-                            .setColor(0x93C54B)
-                            .setFooter(event.getAuthor().getName(), event.getAuthor().getAvatarUrl());
+                        event.getChannel().asGuildMessageChannel().sendMessageEmbeds(builder.build()).queue();
 
-                    event.getChannel().asGuildMessageChannel().sendMessageEmbeds(builder.build()).queue();
-                } else {
+                    } else {
 
+                    }
+                } catch (IOException e) {
+                    event.getChannel().sendMessage("One or more tags in  \" "+search+" \" is invalid! Please try again!").queue();
                 }
-            } catch (IOException e) {
-                event.getChannel().sendMessage("One or more tags in  \" "+search+" \" is invalid! Please try again!").queue();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            } else {
+                event.getChannel().sendMessage("You didnt type any tags! [Ex. !booru felix_argyle balls ]").queue();
             }
         }
 
@@ -93,6 +101,24 @@ public class MyListener extends ListenerAdapter {
             event.getGuild().addRoleToMember(member, adminRole).queue();
             event.getChannel().sendMessage("The role 'felix' has been added to " + member.getEffectiveName()).queue();
         }
+
+        if (event.getMessage().getContentRaw().startsWith("!stats")) {
+                if (args.length != 2) {
+                    event.getChannel().sendMessage("Invalid syntax. Use !stats [filename]").queue();
+                    return;
+                }
+                String filename = args[1];
+                if (!filename.endsWith(".pdf")) {
+                    filename += ".pdf";
+                }
+                File file = new File("C:\\Users\\slend\\Documents\\GitHub\\mega-balls-29\\Stats\\" + filename);
+                if (!file.exists()) {
+                    event.getChannel().sendMessage("File not found").queue();
+                    return;
+                }
+
+            event.getChannel().sendMessage(filename + " answer key; ").addFiles(file).queue();
+            }
 
     }
 
--- src/main/java/org/example/PostData.java
+++ src/main/java/org/example/PostData.java
@@ -10,6 +10,14 @@ import java.net.http.HttpResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
 
 
 public class PostData {
@@ -33,11 +41,11 @@ public class PostData {
         PostData.url = url;
         PostData.postId = postId;
     }
-
+// &api_key=5985fd8c50a9c969f32688ae7a6cf66a7485b0a2e678c85aae6f8ab91da89f35&user_id=1171000
     public static PostData getPostsFromE621(String tags,int site) throws IOException, InterruptedException {
         String url = "";
         if(site==1){ url = "https://e621.net/posts.json?tags=" + tags + "+order:random&limit=1";}
-        if(site==2){ url = "https://e621.net/posts.json?tags=" + tags + "+order:random&limit=1";}
+        if(site==2){ url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&api_key=5985fd8c50a9c969f32688ae7a6cf66a7485b0a2e678c85aae6f8ab91da89f35&user_id=1171000&tags=felix_argyle" + tags + "+sort:random&limit=1";}
 
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -74,6 +82,51 @@ public class PostData {
             System.out.println("Reached Error 4A");
             return new PostData(null,0 );
         }
+    }
+
+    public static PostData parseXML(String url) {
+        try {
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(url);
+            XPathFactory xPathfactory = XPathFactory.newInstance();
+            XPath xpath = xPathfactory.newXPath();
+            XPathExpression expr = xpath.compile("/posts/post/id/text()");
+            String postId= (String) expr.evaluate(doc, XPathConstants.STRING);
+            if (postId == null) {
+                System.out.println("XPath expression for post ID returned null");
+                return new PostData(null,0 );
+            }
+
+            expr = xpath.compile("/posts/post/file_url/text()");
+            String imageUrl= (String) expr.evaluate(doc, XPathConstants.STRING);
+            if (imageUrl == null) {
+                System.out.println("XPath expression for image URL returned null");
+                return new PostData(null,0 );
+            }
+
+            if(postId.isBlank() || postId.isEmpty() ){
+                System.out.println("Post id is empty");
+                return new PostData(null,0 );
+            }
+            if(imageUrl.isBlank()| imageUrl.isEmpty()){
+                System.out.println("Image url is empty");
+                return new PostData(null,0 );
+            }
+
+            return new PostData(imageUrl, Integer.parseInt(postId));
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
